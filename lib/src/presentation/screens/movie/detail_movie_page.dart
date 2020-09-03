15a94f539d2508b2c09d8187d93eb5cbfccae2dc
Gregory Antonio Iscala Caicedo <greko11maister@gmail.com>
2020-09-02 22:21:19 -0500

    maquetado de la pantalla de detalle de pelicula

--- lib/src/presentation/screens/movie/detail_movie_page.dart
+++ lib/src/presentation/screens/movie/detail_movie_page.dart
@@ -1,98 +1,202 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:movieapp/src/presentation/screens/components/actor/actor_component.dart';
+
 class DetailMoviePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF2C3848),
-      body: Column(children: <Widget>[
-        Placeholder(fallbackHeight: 320,),
-        Padding(
-          padding: const EdgeInsets.only(left: 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
             children: <Widget>[
-            Text("Aquaman", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),),
-              SizedBox()
-          ],),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left:30, top: 10,right: 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(8),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.only(bottom: 27),
+                height: 320,
                 decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.3),
-                  borderRadius: BorderRadius.all(Radius.circular(50))
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://sm.ign.com/t/ign_es/news/a/aquaman-2-/aquaman-2-officially-in-development_dvy4.1200.jpg"),
+                        fit: BoxFit.cover)),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                        IconButton(
+                            icon: Icon(
+                              MdiIcons.heartOutline,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            })
+                      ],
+                    )
+                  ],
                 ),
-                child: Text("WATCH NOW", style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold, color: Colors.white),)),
-            Wrap(
-              spacing: 5,
-              children: [
-                Icon(Icons.star, color: Colors.yellowAccent, size: 14,),
-                Icon(Icons.star, color: Colors.yellowAccent, size: 14),
-                Icon(Icons.star, color: Colors.yellowAccent, size: 14),
-                Icon(Icons.star, color: Colors.yellowAccent, size: 14),
-                Icon(Icons.star, color: Colors.grey, size: 14),
-              ],)
-          ],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(30),
-          child: Text("In 1985 Maine, lighthouse keeper Thomas Curry rescues Atlanna, the queen of the underwater nation of Atlantis, during a storm. They eventually fall in love and have a son named Arthur, who is born with the power to communicate with marine lifeforms.",
-          style: TextStyle(color: Colors.white.withOpacity(0.7), fontSize: 13),
-            textAlign: TextAlign.left,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Aquaman",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    SizedBox()
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, top: 10, right: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.3),
+                            borderRadius: BorderRadius.all(Radius.circular(50))),
+                        child: Text(
+                          "WATCH NOW",
+                          style: TextStyle(
+                              fontSize: 11,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        )),
+                    Wrap(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellowAccent,
+                          size: 14,
+                        ),
+                        Icon(Icons.star, color: Colors.yellowAccent, size: 14),
+                        Icon(Icons.star, color: Colors.yellowAccent, size: 14),
+                        Icon(Icons.star, color: Colors.yellowAccent, size: 14),
+                        Icon(Icons.star, color: Colors.grey, size: 14),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Text(
+                  "In 1985 Maine, lighthouse keeper Thomas Curry rescues Atlanna, the queen of the underwater nation of Atlantis, during a storm. They eventually fall in love and have a son named Arthur, who is born with the power to communicate with marine lifeforms.",
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.7), fontSize: 13),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+
+              // Actor
+              Container(
+                height: 110,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, i) {
+                      return ActorComponent(
+                        avatarUrl: "",
+                        name: "Jason Momoa",
+                      );
+                    }),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, top: 10, bottom: 8),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Studio",
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: Text(
+                        "Warner Bros.",
+                        style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white.withOpacity(0.7)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, bottom: 8),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Genre",
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: Text(
+                        "Action, Adventure, Fantasy",
+                        style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white.withOpacity(0.7)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, bottom: 8),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Release",
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: Text(
+                        "2018",
+                        style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white.withOpacity(0.7)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-
-        // Actor
-        Container(
-          height: 110,
-          child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-              itemCount: 10,
-              itemBuilder: (context, i){
-            return ActorComponent(
-              avatarUrl: "",
-              name: "Jason Momoa",
-            );
-          }),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 30,top: 10, bottom: 8),
-          child: Row(children: <Widget>[
-            Text("Studio", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Colors.white),),
-            Padding(
-              padding: const EdgeInsets.only(left:24),
-              child: Text("Warner Bros.", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400, color: Colors.white.withOpacity(0.7)),),
-            )
-          ],),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 30, bottom: 8),
-          child: Row(children: <Widget>[
-            Text("Genre", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Colors.white),),
-            Padding(
-              padding: const EdgeInsets.only(left:24),
-              child: Text("Action, Adventure, Fantasy", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400, color: Colors.white.withOpacity(0.7)),),
-            )
-          ],),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 30, bottom: 8),
-          child: Row(children: <Widget>[
-            Text("Release", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: Colors.white),),
-            Padding(
-              padding: const EdgeInsets.only(left:24),
-              child: Text("2018", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400, color: Colors.white.withOpacity(0.7)),),
-            )
-          ],),
-        ),
-      ],),
+      ),
     );
   }
 }
